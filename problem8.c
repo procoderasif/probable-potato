731b280a60c773561842263cbf4d8d5ac18da4a0
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-08-10 03:36:44 +0600

    oby
    yba

---
+++ problem8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+{
+    int A,B;
+    double C,SALARY;
+    scanf("%d%d%lf",&A,&B,&C);
+    SALARY=B*C;
+    printf("NUMBER = %d\nSALARY = U$ %.2lf/n",A,SALARY);
+
+    return 0;
+}
